13f3c7cd9b9bceccf120501eece1512f02ff0305
Alex Prieu <daprieu@gmail.com>
2022-01-11 11:56:41 -0600

    first commit of new pokedex app

---
+++ src/components/AppNav.js
@@ -0,0 +1,31 @@
+import { AppBar, Toolbar, Typography } from '@mui/material'
+import React from 'react'
+import { makeStyles } from '@mui/styles'
+import { Link } from 'react-router-dom'
+
+const useStyles = makeStyles((theme) => ({
+    //This is not working added style prop to <AppBar/>
+    // AppBar: {
+    //     backgroundColor: "pink",
+    // },
+    link: {
+        textDecoration: "none",
+    },
+    title: {
+        cursor: 'pointer',
+        color: 'white',
+    }
+}))
+
+export default function AppNav() {
+    const classes = useStyles()
+    return (
+        <AppBar className={classes.AppBar} style={{backgroundColor: 'black' }} position='fixed' >
+            <Toolbar>
+                <Link to="/" className={classes.link}>
+                    <Typography className={classes.title} variant='h6'>Pokedex</Typography>
+                </Link>
+            </Toolbar>
+        </AppBar>
+    )
+}
