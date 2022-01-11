6142c9e7c64d29943c00ae65b2416105753ac78b
Alex Prieu <daprieu@gmail.com>
2022-01-11 14:19:46 -0600

    App is fetching Pokemon using react axios and then map through data to then display names on webpage

--- src/containers/Pokedex.js
+++ src/containers/Pokedex.js
@@ -1,11 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Box, CircularProgress } from '@mui/material'
+import axios from 'axios'
+import { IMAGE_API_URL, POKEMON_API_URL } from '../config'
 
 export default function Pokedex() {
+    let a
+    const [pokemonDataState, setPokemonDataState] = useState([])
+    console.log('pokemonDataState', pokemonDataState)
+    useEffect(() => {
+        axios.get(`${POKEMON_API_URL}?limit=800`).then((response) => {
+            if(response.status >= 200 && response.status < 300){
+                const { results } = response.data
+                let newPokemonData = []
+                results.forEach((pokemon, index) => {
+                    index++
+                    let pokemonObject = {
+                        id: index,
+                        url: `${IMAGE_API_URL}${index}.png`,
+                        name: pokemon.name
+                    }
+                    newPokemonData.push(pokemonObject)
+                });
+                setPokemonDataState(newPokemonData)
+            }
+        })
+    }, [])
     return (
         <>
-        <div>
-            <h1>Hello World</h1>
-        </div>
+        <Box>
+            {pokemonDataState ? pokemonDataState.map((pokemon) => {
+                return <h1 key={pokemon.id}>{pokemon.name}</h1>
+            }) : <CircularProgress style={{marginTop: 100 }}/>}
+        </Box>
         </>
     )
 }
